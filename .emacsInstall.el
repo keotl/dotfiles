7ef075d282466546eaeb018c7b831681b29de83b
KEOTL <kento.otomo-lauzon.1@ulaval.ca>
2017-04-30 17:16:52 -0400

    Added sr-speedbar s-s

--- .emacsInstall.el
+++ .emacsInstall.el
@@ -1,3 +1,3 @@
-(defvar packages '(ac-c-headers ac-clang ace-window auto-complete avy company-anaconda anaconda-mode company-go company-irony company-irony-c-headers company-shell company-web company gh-md go-mode irony magit git-commit magit-popup popup pos-tip pythonic f spaceline s powerline sublimity web-completion-data with-editor dash async xresources-theme yasnippet dashboard projectile))
+(defvar packages '(ac-c-headers ac-clang ace-window auto-complete avy company-anaconda anaconda-mode company-go company-irony company-irony-c-headers company-shell company-web company gh-md go-mode irony magit git-commit magit-popup popup pos-tip pythonic f spaceline s powerline sublimity web-completion-data with-editor dash async xresources-theme yasnippet dashboard projectile sr-speedbar))
 
 (mapcar 'package-install packages)
