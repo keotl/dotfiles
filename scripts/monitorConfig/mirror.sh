5b7865bb0e78eafcebf7ce96806e3322d4f1bf99
Kento Lauzon <kento.lauzon@ligature.ca>
2018-01-20 16:07:50 -0500

    Mirror scaling external display retinaMBP

--- scripts/monitorConfig/mirror.sh
+++ scripts/monitorConfig/mirror.sh
@@ -1,1 +1,2 @@
-xrandr --output LVDS1 --mode 1280x800 --output DVI1 --mode 1280x800
+#!/bin/sh
+xrandr --output HDMI-2 --mode 1280x800 --scale-from 2560x1600 --same-as eDP-1 --output eDP-1 --mode 2560x1600
