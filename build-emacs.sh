81c95aae5914d1d303e547ff09b9615e02095a04
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-12-05 18:53:28 -0500

    libxml2 in build-emacs

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev gnutls-dev libgtk-3-dev
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev gnutls-dev libgtk-3-dev libxml2-dev
 git clone https://github.com/emacs-mirror/emacs.git
 cd emacs
 git checkout emacs-26.3
