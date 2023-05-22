f518d030f19129829031f86e17fddb1ab24b3dd0
Kento Lauzon <kento.lauzon@ligature.ca>
2023-05-22 20:45:42 +0100

    home docking mode

---
+++ dock.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#sudo bash -c "echo 0 > /sys/bus/usb/devices/3-6/authorized"
+#sudo bash -c "echo 1 > /sys/bus/usb/devices/3-6/authorized"
+
+
+xrdb ~/.Xresources-monitor
+sh ~/.screenlayout/home.sh
+sh ~/.startup
