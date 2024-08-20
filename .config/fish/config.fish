a49a25bf2ae4fd0cbdc2144d196356d3d94edfd3
Kento Lauzon <kento@lauzon.co.uk>
2024-08-20 17:46:36 +0100

    simplify fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-nvm use 14.15.1
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
--- fish/functions/fish_greeting.fish
+++
@@ -1,3 +0,0 @@
-function fish_greeting
-	fortune ~/apps/irulan/mp -s fortunes literature
-end
